cf419d25778d25ae72ea38c158fb1af2e3778e16
Łukasz Kubica <kubicalukasz92@gmail.com>
2019-03-20 12:11:07 +0100

    only routebutton

--- ios/RNVolumeSlider/VolumeSliderManager.m
+++ ios/RNVolumeSlider/VolumeSliderManager.m
@@ -8,19 +8,4 @@ RCT_EXPORT_MODULE()
     return [[VolumeSlider alloc] init];
 }
 
-RCT_EXPORT_VIEW_PROPERTY(minimumTrackTintColor, UIColor);
-RCT_EXPORT_VIEW_PROPERTY(maximumTrackTintColor, UIColor);
-RCT_EXPORT_VIEW_PROPERTY(thumbTintColor, UIColor);
-RCT_EXPORT_VIEW_PROPERTY(onValueChange, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(thumbImage, UIImage);
-RCT_EXPORT_VIEW_PROPERTY(showsRouteButton, BOOL);
-RCT_CUSTOM_VIEW_PROPERTY(thumbSize, RCTthumbSize, VolumeSlider) {
-  NSDictionary *thumbSize = (NSDictionary *) json;
-  
-  [view setThumbWidth:[thumbSize[@"width"] floatValue]];
-  [view setThumbHeight:[thumbSize[@"height"] floatValue]];
-  
-  [view setThumb];
-}
-
 @end
