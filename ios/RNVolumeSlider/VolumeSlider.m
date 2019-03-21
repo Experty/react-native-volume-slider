d68ea551d35c59769eca00d51506233e65751fa9
Łukasz Kubica <kubicalukasz92@gmail.com>
2019-03-21 13:25:24 +0100

    remove all images from all states

--- ios/RNVolumeSlider/VolumeSlider.m
+++ ios/RNVolumeSlider/VolumeSlider.m
@@ -6,6 +6,14 @@
   if (self = [super init]) {
     self.showsVolumeSlider = NO;
     self.showsRouteButton = YES;
+
+    [self setRouteButtonImage:nil forState:UIControlStateNormal];
+    [self setRouteButtonImage:nil forState:UIControlStateHighlighted];
+    [self setRouteButtonImage:nil forState:UIControlStateApplication];
+    [self setRouteButtonImage:nil forState:UIControlStateSelected];
+    [self setRouteButtonImage:nil forState:UIControlStateReserved];
+    [self setRouteButtonImage:nil forState:UIControlStateDisabled];
+    [self setRouteButtonImage:nil forState:UIControlStateFocused];
   }
   return self;
 }
